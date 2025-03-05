3686fcc9d8a73f4c9e012126be2a9651133d4b2a
DasSagor <dassagor888@gmail.com>
2025-03-05 21:04:52 +0600

    1st day done

--- first.js
+++ first.js
@@ -1,3 +1,55 @@
-let x = '33abc'
-let y = Number(x)
-console.log(y)
+// let x = '33abc'
+// let y = Number(x)
+// console.log(y)
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+// console.log(null===0)
+
+// const num = 234.677
+// console.log(num.toPrecision(3))
+// console.log(num.toPrecision(4))
+// console.log(num.toPrecision(5))
+
+// const val = 10000000
+// console.log(val.toLocaleString('en-IN'));
+
+// console.log(((Math.random()*10)).toFixed());
+ 
+
+// const arr1 = ['sagor', 'shuvo','Indrojit','Minoti']
+// const arr2 = ['Prionty','Pria',]
+
+// const SpreadArr = [...arr1,...arr2]
+// console.log(SpreadArr);
+
+// const arr = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// console.log(arr.flat(Infinity));
+// console.log(arr.flat(1));
+
+// function add(num1,num2){
+//     return `sum of numbers : ${num1 + num2}`
+// }
+// console.log(add(2,4));
+
+
+// function addNum(){
+//     console.log("add");
+//     return 100;
+// }
+// function subNum(){
+//     console.log("sub");
+//     return 200;
+// }
+// // let a = ()
+// console.log(addNum(),subNum());
+
+
+// let num = 2
+// let a = !--num
+// let b = !--num
+// console.log(a,b);
+
+// console.log(+"555");
+// console.log(+555);
